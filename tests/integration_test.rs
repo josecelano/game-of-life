c51a9cfc069f0d4d59d9e5c3ec4199236223ab01
Jose Celano <josecelano@gmail.com>
2022-11-05 21:31:35 +0000

    fix: golden test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,11 @@
+use std::fs;
+
 use game_of_life::game::play;
 
 #[test]
 fn test_add() {
-    assert_eq!(play(), "⬛⬜⬛\n⬛⬛⬜\n⬜⬜⬜\n");
+    let expected_output = fs::read_to_string("./tests/fixtures/expected_output.txt")
+        .expect("Missing snapshot of output in golden test");
+
+    assert_eq!(play(), expected_output);
 }
